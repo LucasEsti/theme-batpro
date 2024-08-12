1d7b8ec002d46d70eb35617bda33b820da5e9ff8
Lucas Angie <lie.lucas.razafindrambo@esti.mg>
2024-08-12 15:48:15 +0300

    modif mineur

--- footer-client.php
+++ footer-client.php
@@ -1,7 +1,4 @@
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    
+
     <?php
     $source = get_bloginfo("template_url");
 
@@ -437,5 +434,3 @@
     </script>
     
     <script src="<?php echo $source; ?>style/chatbox.js"></script>
-</body>
-</html>
