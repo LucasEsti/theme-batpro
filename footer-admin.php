204065aba4852495b4f8b82e870603f035ed75d1
Lucas Angie <lie.lucas.razafindrambo@esti.mg>
2024-08-13 11:12:36 +0300

    modif base

--- footer-admin.php
+++ footer-admin.php
@@ -208,7 +208,7 @@
                 var messageContainer = document.getElementById('messageContainer');
                 
                 createMessageSection(data.from, data.from);
-                createInput(data.from, true);
+                createInput(data.from, false);
                 var messageDisplay = document.getElementById('content-' + data.from);
                 var messageDiv = document.createElement('div');
                 
