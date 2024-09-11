b14819e75725ac43aa242387924e59974c2ea818
Lucas Angie <lie.lucas.razafindrambo@esti.mg>
2024-09-11 16:38:37 +0300

    style

--- header-admin.php
+++ header-admin.php
@@ -22,7 +22,7 @@ $uploadsUrl = $source . "/realtime-batpro/uploads/";
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
 <link rel="stylesheet" href="<?php bloginfo("template_url");  ?>/realtime-batpro/direct-messaging/dist/style.css">
     <style>
-                .listPeople {
+                #listPeople {
             overflow-x: scroll;
             height: 75%;
             position: relative;
