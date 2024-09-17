125df0fccbfd3ecc6ae853ccd686de445470bf27
Lucas Angie <lie.lucas.razafindrambo@esti.mg>
2024-09-17 09:17:22 +0300

    modif mineur

--- footer-admin.php
+++ footer-admin.php
@@ -29,8 +29,8 @@
             // Fermer le modal et lire l'audio lorsque l'utilisateur clique sur "Oui"
             confirmButton.onclick = function() {
                 modal.style.display = 'none';
-                var audio = new Audio('path/to/your/audiofile.mp3');
-                audio.play().catch(function(error) {
+//                var audio = new Audio('path/to/your/audiofile.mp3');
+//                audio.play().catch(function(error) {
 //                    alert('Voulez-vous activer les notifications sonores?');
                 });
             }
