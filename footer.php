285caf669af7cf621485164d17da776247d51e05
Lucas Angie <lie.lucas.razafindrambo@esti.mg>
2024-12-09 10:26:24 +0300

    ads config

--- footer.php
+++ footer.php
@@ -712,6 +712,17 @@ if (get_field('version_page') == "en") {
         
     </script>
     
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-10942608044">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-10942608044');
+</script>
+    
 </body>
 </html>
     
