0b0ac6737fb17f5d88fc64035e9ae9690e276f5a
Lucas Angie <lie.lucas.razafindrambo@esti.mg>
2025-02-26 16:22:33 +0300

    gtag delete

--- footer.php
+++ footer.php
@@ -752,34 +752,6 @@ if (get_field('version_page') == "en") {
         
     </script>
     
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-10942509957"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-10942509957');
-</script>
-
-<!-- Google tag (gtag.js) event - delayed navigation helper -->
-<script>
-  // Helper function to delay opening a URL until a gtag event is sent.
-  // Call it in response to an action that should navigate to a URL.
-  function gtagSendEvent(url) {
-    var callback = function () {
-      if (typeof url === 'string') {
-        window.location = url;
-      }
-    };
-    gtag('event', 'contact_us', {
-      'event_callback': callback,
-      'event_timeout': 2000,
-      // <event_parameters>
-    });
-    return false;
-  }
-</script>
 
     
 </body>
