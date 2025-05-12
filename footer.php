88f8b8c41e9c79abb46e339c644a2af2668ebd06
Lucas Angie <lie.lucas.razafindrambo@esti.mg>
2025-05-12 15:39:42 +0300

    google tag ads

--- footer.php
+++ footer.php
@@ -751,7 +751,15 @@ if (get_field('version_page') == "en") {
         }
         
     </script>
-    
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BH2S460TXC"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BH2S460TXC');
+</script>
 
     
 </body>
